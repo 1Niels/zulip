8d5b7024624963503327df6b9f29205f0adf66e3
Niels Terwiesch <nterwiesch@gmail.com>
2017-01-03 03:37:18 +0000

    add: help request

--- contrib_bots/lib/interpreterbot.py
+++ contrib_bots/lib/interpreterbot.py
@@ -10,7 +10,16 @@ class InterpreterBotHandler(object):
         return '''
             This plugin will allow users to flag messages
             containing code to be evaluated. Users should
-            preface messages with "@InterpreterBot".
+            preface messages with "@InterpreterBot". The
+            format of the message should be as follows,
+            "@InterpreterBot [language] ```[code]```".
+            Currently language is limited to python3,
+            python, ruby, php, java, go, nodejs, csharp (c#),
+            fsharp (f#), cpp (c++), cpp (c++11), and c.
+            
+            Alternatively users may send "@InterpreterBot
+            help" to display help information about the
+            bot.
             '''
 
     def triage_message(self, message, client):
@@ -25,28 +34,41 @@ class InterpreterBotHandler(object):
             return False
         is_follow_up = (original_content.startswith('@InterpreterBot'))
         return is_follow_up
+        
     
-    def handle_input(InterpreterBotHandler, original_content, original_sender):
-        original_content = original_content.split('```')
-        code = original_content[1]
-        language = original_content[0].split(' ')[1]
-        
-        
-        
-        output = """lang: %s, code:\n```%s```""" % (language, code)
-        new_content = output
-        return new_content
+    def handle_input(InterpreterBotHandler, original_content, original_sender, message_id, stream, topic):
+        supported_languages = ['python3','python', 'ruby', 'php','java','go','nodejs','csharp','fsharp','cpp','cpp11','c']
+        try:
+            original_content = original_content.split('```')
+            code = original_content[1]
+        except:
+            if original_content[0].lower().find('help') >= 0:
+                language = "help"
+            else:
+                language = "unknown"
+
+        if language in supported_languages:
+            output = """lang: %s, code:\n```%s```""" % (language, code)
+            bot_message = """[{}](http://1niels.zulipdev.org:9991/#narrow/near/{}/stream/{}/topic/{}) - {}: \n``` {}```
+            """.format(original_sender,message_id, stream, topic, language, code)
+        elif language.lower() == "help":
+            bot_message = 'Interpreter Bot supports the following languages: python'
+        else:
+            bot_message = original_content
+        return bot_message
     
     def handle_message(InterpreterBotHandler, message, client, state_handler):
         original_content = message['content']
         original_sender = message['sender_full_name']
-        #code_output = InterpreterBotHandler.handle_input(original_content, original_sender)
-        code_output = "Output for [%s' message](http://1niels.zulipdev.org:9991/#narrow/near/%s/stream/%s/topic/%s)" % (message['sender_full_name'],message['id'], message['display_recipient'], message['subject'])
+        message_id = message['id']
+        stream = message['display_recipient']
+        topic = message['subject']
+        bot_message = InterpreterBotHandler.handle_input(original_content, original_sender, message_id, stream, topic)
         client.send_message(dict(
             type='stream',
             to=message['display_recipient'],
             subject=message['subject'],
-            content= code_output,
+            content= bot_message,
         ))
 
 handler_class = InterpreterBotHandler
